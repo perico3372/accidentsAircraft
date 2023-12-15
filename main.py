b27d51220a5f7cabdab0cfcdee86f4743165bfee
perico3372 <perico3372@gmail.com>
2023-12-15 10:43:35 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+st.markdown("---")
+st.markdown("Proyecto Individual")
+st.markdown("---")
+st.markdown("Data Analitics")
+st.markdown("---")
+st.markdown("Perez, Pablo Jorge")
+st.markdown("---")
